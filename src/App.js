96f7eedf3f156273fdda06d11eb024f3e76f7ee1
i-sanyam <sanyam.aggarwal@jungleworks.com>
2021-06-14 00:05:30 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import logo from './logo_main.png';
 import bg from './bg.jpg';
-import './App.css';
 
 function App() {
   return (
-    <div id='frame'>
-      {/* <h3>Website Under Construction! Please visit later.</h3> */}
-      <img id= 'bg' src={bg} className='ui huge centered image' alt='LINCON background' />
-      <img id='mainLogo' src={logo} className='ui medium centered image' alt='LINCON logo' />
+    <div>
+      COOL
     </div>
 
   );
